0c1ebb0bd9850e43446327bef00fd40f860afe8d
Sneha <sneharoy90737@gmail.com>
2026-01-08 21:11:40 +0530

    prepare base model added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -9,21 +9,21 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
-#
-#
-# @dataclass(frozen=True)
-# class PrepareBaseModelConfig:
-#     root_dir: Path
-#     base_model_path: Path
-#     updated_base_model_path: Path
-#     params_image_size: list
-#     params_learning_rate: float
-#     params_include_top: bool
-#     params_weights: str
-#     params_classes: int
-#
-#
-#
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+
+
+
 # @dataclass(frozen=True)
 # class TrainingConfig:
 #     root_dir: Path
