711d170abb05a470b0d93aef92d927e1bee39013
Sneha <sneharoy90737@gmail.com>
2026-01-08 22:21:28 +0530

    model training added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,9 +1,9 @@
 from cnnClassifier.constants import *
-# import os
+import os
 from cnnClassifier.utils.common import read_yaml, create_directories, save_json
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
-                                                PrepareBaseModelConfig)
-                                                # TrainingConfig,
+                                                PrepareBaseModelConfig,
+                                                TrainingConfig)
                                                 # EvaluationConfig)
 
 
@@ -16,6 +16,7 @@ class ConfigurationManager:
         self.params = read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
+
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
@@ -30,6 +31,7 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
 
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
@@ -48,29 +50,31 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
-#
-#     def get_training_config(self) -> TrainingConfig:
-#         training = self.config.training
-#         prepare_base_model = self.config.prepare_base_model
-#         params = self.params
-#         training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney-ct-scan-image")
-#         create_directories([
-#             Path(training.root_dir)
-#         ])
-#
-#         training_config = TrainingConfig(
-#             root_dir=Path(training.root_dir),
-#             trained_model_path=Path(training.trained_model_path),
-#             updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
-#             training_data=Path(training_data),
-#             params_epochs=params.EPOCHS,
-#             params_batch_size=params.BATCH_SIZE,
-#             params_is_augmentation=params.AUGMENTATION,
-#             params_image_size=params.IMAGE_SIZE
-#         )
-#
-#         return training_config
-#
+
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney-ct-scan-image")
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
+
+
 #     def get_evaluation_config(self) -> EvaluationConfig:
 #         eval_config = EvaluationConfig(
 #             path_of_model="artifacts/training/model.h5",
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -24,19 +24,19 @@ class PrepareBaseModelConfig:
 
 
 
-# @dataclass(frozen=True)
-# class TrainingConfig:
-#     root_dir: Path
-#     trained_model_path: Path
-#     updated_base_model_path: Path
-#     training_data: Path
-#     params_epochs: int
-#     params_batch_size: int
-#     params_is_augmentation: bool
-#     params_image_size: list
-#
-#
-#
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
+
+
+
 # @dataclass(frozen=True)
 # class EvaluationConfig:
 #     path_of_model: Path
